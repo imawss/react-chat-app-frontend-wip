3ead1e5cf79e84de9d739c32c3d8f10e43125184
✦aras <arasarkan9@gmail.com>
2023-01-10 18:17:25 +0300

    login component and css file created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LoginPages from './pages/LoginPages';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
   {
     path:"/",
     element: <App/>
+  },
+  {
+    path:"/login",
+    element: <LoginPages/>
   }
 ])
 
---
+++ src/pages/LoginPages.js
@@ -0,0 +1,15 @@
+import { Container, Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useRef, useState } from 'react';
+import Axios from "axios";
+import './Login.css';
+
+function LoginPages() {
+  return (
+    <Container id='main-container' className='d-grid h-100'>
+        
+    </Container>
+  )
+}
+
+export default LoginPages;
