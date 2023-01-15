5212dfa6f2af9f60ca86fb6775b636d115a08ce9
✦aras <arasarkan9@gmail.com>
2023-01-15 16:50:43 +0300

    little style change

--- src/pages/404Page.js
+++ src/pages/404Page.js
@@ -6,7 +6,7 @@ function ErrorPage() {
     <Container id='main-container' className='d-grid h-100'>
         <div className='error-bg'>
             <h1 id='error-c'>404</h1>
-            <h2 id='error-d'>Sorry, the page you're looking for doesn't exist.</h2> 
+            <h2 id='error-d'>Sorry, the page you are looking for doesn't exist.</h2> 
         </div>
     </Container>
   )
