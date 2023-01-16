5d78ef375ba95426b433d6972f307def02895cf1
✦aras <arasarkan9@gmail.com>
2023-01-16 14:08:32 +0300

    auth check

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import { Container, Form, Button } from 'react-bootstrap';
-import { Link, Navigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { useRef, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
@@ -10,7 +10,16 @@ import './Home.css';
 
 function HomePage() {
   
+  const navigate = useNavigate();
+
   const checkAuth = () => {
+    Axios.get("http://localhost:8000/apiv1/users/getAuth")
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      navigate("/login");
+    });
     console.log("func test!");
   }
 
